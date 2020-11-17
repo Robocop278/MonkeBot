11aea2a357e15ced38626ed5df1f626406b38162
Nia <nia@pop-os.localdomain>
2020-11-16 22:57:25 -0800

    shorten phasmophobia trigger word and fix valid video list

--- bot.js
+++ bot.js
@@ -184,7 +184,7 @@ client.on('message', msg => {
         {
             mYouTube.playYoutubeSound(msg, 'https://www.youtube.com/watch?v=EcsPhdyZM4k');
         }
-        else if (/phasmophobia/i.test(msg.content))
+        else if (/phasmo/i.test(msg.content))
         {
             mYouTube.playYoutubeSound(msg, 'https://www.youtube.com/watch?v=' + phasmo.getRandomSpiritBoxVideoID());
         }
