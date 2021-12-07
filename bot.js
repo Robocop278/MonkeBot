6b6956084256a25e0457f3473402779e6a4c3811
Christian Gutierrez <christian.gutierrez63@yahoo.com>
2021-12-06 21:41:09 -0800

    Add gif reaction to Charles command

--- bot.js
+++ bot.js
@@ -360,6 +360,7 @@ client.on('message', msg => {
         }
         else if (/Charles|pull\s*up|capybara/i.test(msg.content)) {
             mYouTube.playYoutubeSound(msg, 'https://youtu.be/v8Q4BSgBkRc');
+            msg.channel.send('https://c.tenor.com/g05O_S6b0f4AAAAd/capybara-ok-i-pull-up.gif');
         }
 
 
