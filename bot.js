a158a96e72227840d41ef2270d67b9baecef75ab
Kevin Capotosto <36879680+theemadbro@users.noreply.github.com>
2022-03-14 21:32:23 -0400

    trying 4th link

--- bot.js
+++ bot.js
@@ -783,7 +783,7 @@ client.on('message', msg => {
             mYouTube.playSoundOgg(msg, BOSS_SOUNDS[2]);
         }
         else if (/very\s*cool/i.test(msg.content)) {
-            mYouTube.playSoundOgg(msg, 'https://monke.s3.amazonaws.com/very+cool.mp3');
+            mYouTube.playSoundOgg(msg, 'https://monke.s3.us-east-1.amazonaws.com/very%20cool.mp3');
         }
         else if (/^boner\s*-3/i.test(msg.content)) {
             mYouTube.playSoundOgg(msg, BONER_SOUNDS[0]);
