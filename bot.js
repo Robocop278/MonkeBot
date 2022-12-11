b84ed10fa7cc6d3b2c3f723744bdb98a6be51d30
Christian Gutierrez <christian.gutierrez63@yahoo.com>
2022-12-11 14:52:26 -0800

    Tweak ha he to use space instead of wildcard in regex

--- bot.js
+++ bot.js
@@ -1394,7 +1394,7 @@ client.on('message', msg => {
     else if (/you have (failed|lost) the challenge/i.test(msg.content)) {
         mYouTube.playSound(msg, 'https://monke.s3.amazonaws.com/gnome/GNOME CHALLENGE.mp3');
     }
-    else if (/(gnome|ha.*he)/i.test(msg.content)) {
+    else if (/(gnome|ha\s*he)/i.test(msg.content)) {
         mYouTube.playSound(msg, 'https://monke.s3.amazonaws.com/gnome/GNOME HAHE.mp3');
     }
     else if (/caught in 4k/i.test(msg.content)) {
