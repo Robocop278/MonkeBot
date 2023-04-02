1be84078ebedd39f4714fd2ab308bb4383bef96b
Kevin Lisbin <kevin.lisbin@gmail.com>
2023-04-02 00:37:13 -0700

    removing build folder from git

--- build/src/util.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import * as fs from 'fs';
-export declare const readFilePromise: typeof fs.readFile.__promisify__;
-export declare function readJsonPromise(pathToFile: string): Promise<any>;
