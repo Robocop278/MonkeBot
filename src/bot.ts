0e827a43516c1bd8fd2966461a5f6f2646b82aab
Kevin Lisbin <kevin.lisbin@gmail.com>
2023-04-02 01:42:51 -0700

    setting a new nickname and avatar for monke

--- src/bot.ts
+++ src/bot.ts
@@ -1,3 +1,5 @@
+import {Guild} from 'discord.js';
+import {ActivityType} from 'discord.js';
 import {Client, Events, GatewayIntentBits, Partials} from 'discord.js';
 require('json5/lib/register');
 // eslint-disable-next-line node/no-unpublished-require
@@ -12,13 +14,20 @@ const client = new Client({
 });
 
 client.once(Events.ClientReady, (c: Client<boolean>) => {
+  client.user?.setActivity('with type safety!', {type: ActivityType.Playing});
+  client.user?.setAvatar('avatar_images/monkeBot_hardHat.png');
+  client.guilds.cache
+    .get(constantants.CHANNEL_MAIN)
+    ?.members.cache.get('690351869650010333')
+    ?.setNickname('🚧 Monke 2.0 🚧');
   console.log(`Ready! Logged in as ${c.user?.tag}`);
 });
 
 client.on(Events.MessageCreate, message => {
-  console.log(message.content);
+  // We want to ignore all messages that come from monke itself
+  if (message.author.id === '690351869650010333') return;
+
+  console.log(`Message from ${message.author.username}: ${message.content}`);
 });
 
 client.login(constantants.PRIVATE_KEY);
-
-console.log('TEST');
