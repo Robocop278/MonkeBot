1c12141d794594ecab13b60424fe35aaad84e1cf
Christian Gutierrez <christian.gutierrez63@yahoo.com>
2024-04-14 01:00:56 -0700

    me am big dum

--- src/bot.ts
+++ src/bot.ts
@@ -164,10 +164,10 @@ async function processCommand(command: ActionableCommand, message: Message) {
     } else {
       processCommand({
         content: [
-          { reply: true, media_url: 'https://64.media.tumblr.com/fcb630b33a66e2d0a45e76baa59f1e2b/tumblr_o35vmuaRom1s2w9y9o4_500.gif' },
-          { reply: true, media_url: 'https://www.reactiongifs.com/r/trlrky.gif' },
-          { reply: true, media_url: 'https://www.bigfooty.com/forum/media/dr-evil-how-about-no-gif.115792/full' },
-          { reply: true, media_url: 'https://giphy.com/gifs/ForbesTheCulture-fortheculture-forbestheculture-forbes-the-culture-0FPXJfQjetvyOFcOgo' }
+          { reply: true, text_content: 'https://64.media.tumblr.com/fcb630b33a66e2d0a45e76baa59f1e2b/tumblr_o35vmuaRom1s2w9y9o4_500.gif' },
+          { reply: true, text_content: 'https://www.reactiongifs.com/r/trlrky.gif' },
+          { reply: true, text_content: 'https://www.bigfooty.com/forum/media/dr-evil-how-about-no-gif.115792/full' },
+          { reply: true, text_content: 'https://giphy.com/gifs/ForbesTheCulture-fortheculture-forbestheculture-forbes-the-culture-0FPXJfQjetvyOFcOgo' }
         ]
       }, message);
     }
