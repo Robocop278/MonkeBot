9a574ac5078877d447ff6ea09b84302a58304bb5
Christian Gutierrez <christian.gutierrez63@yahoo.com>
2024-12-10 00:36:29 -0800

    Test attach file

--- src/bot.ts
+++ src/bot.ts
@@ -71,10 +71,10 @@ client.once(Events.ClientReady, (c: Client<boolean>) => {
   console.log(`Starting crons`);
 
   // Test cron
-  new CronJob('22 8 10 12 *', async () => {
+  new CronJob('39 8 10 12 *', async () => {
     client.channels.fetch(configs.CHANNEL_LOGS)
       .then(channel => {
-        (channel as TextChannel).send(`https://monke.s3.us-east-1.amazonaws.com/can_u_believe_it_christmas_a_week_away.mp4`);
+        (channel as TextChannel).send({ files: ['https://monke.s3.us-east-1.amazonaws.com/can_u_believe_it_christmas_a_week_away.mp4'] });
       });
   }, null, true);
 
